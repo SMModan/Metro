444b7a796fc1f116d22cab1f2faa23a7f924c2f0
Suhail Ranger <rangersuhail@gmail.com>
2021-06-12 22:50:33 +0530

    Contacts Module

---
+++ src/components/Contacts/BaseComponents.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FloatingEditText, ViewWithTitle } from '../common';
+
+export const CustomerInfo = () => {
+    return (
+        <ViewWithTitle title="Customer Information">
+            <FloatingEditText label={'First Name'} />
+            <FloatingEditText label={'Last Name'}/>
+            <FloatingEditText label={'Customer Name'} />
+        </ViewWithTitle>
+    )
+}
+
+export const ContactInfo = () => {
+    return (
+        <ViewWithTitle title={"Contact Information"}>
+            <FloatingEditText label={'Mobile Number'} />
+            <FloatingEditText label={'Email'} />
+        </ViewWithTitle>
+    )
+}
